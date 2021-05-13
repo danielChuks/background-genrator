cebb668f4c7f511ac1fdf8c1ad63fc4ff019fdb4
niealda <danieluchechukwuchuks1@gmail.com>
2021-05-13 13:32:38 -0700

    added index.html background.css background.js

---
+++ background.js
@@ -0,0 +1,15 @@
+//backgroung color Generator
+var css = document.querySelector("h4");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("grad");
+
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right," + color1.value +  " , " + color2.value + ")";
+    css.textContent = body.style.background + " ; ";
+};
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
