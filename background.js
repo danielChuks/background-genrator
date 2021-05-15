1200f2206cd2d10a4ec700ced2023df243729241
niealda <danieluchechukwuchuks1@gmail.com>
2021-05-14 23:37:00 -0700

    Es6 syntax

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 //backgroung color Generator
-var css = document.querySelector("h4");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("grad");
+const css = document.querySelector("h4");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("grad");
 
 
-function setGradient(){
+const setGradient = () => {
     body.style.background = "linear-gradient(to right," + color1.value +  " , " + color2.value + ")";
     css.textContent = body.style.background + " ; ";
 };
